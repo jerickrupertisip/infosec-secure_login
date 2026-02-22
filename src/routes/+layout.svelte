43b694325c2ade1fbed06e008df57b7f4edd5f92
NexushasTaken <nexushastaken@gmail.com>
2026-02-23 00:48:10 +0800

    feat: change ui design to shadcn

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { ModeWatcher } from 'mode-watcher';
 
 	let { children } = $props();
 </script>
 
+<ModeWatcher />
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+<div class="flex min-h-[100vh] flex-col items-center justify-center">
+  {@render children()}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,15 @@
 <script lang="ts">
-	import AuthForm from './AuthForm.svelte';
+	import { Button } from '$lib/components/ui/button';
+	import { Input } from '$lib/components/ui/input';
+	import * as Field from '$lib/components/ui/field';
+	import {
+		Card,
+		CardContent,
+		CardHeader,
+		CardTitle,
+		CardDescription,
+		CardFooter
+	} from '$lib/components/ui/card';
 
 	let sign_in_form = $state(true);
 
@@ -21,22 +31,41 @@
 	function signUp() {}
 </script>
 
-<AuthForm
-	submit_text={sign_in_form ? 'Sign In' : 'Sign Up'}
-	bind:email_input
-	bind:password_input
-	{onsubmit}
->
-	<p>
-		Already have an account?
-		<button
-			onclick={(e: MouseEvent) => {
-				e.preventDefault();
-				sign_in_form = !sign_in_form;
-			}}
-			class="cursor-pointer font-medium text-indigo-600 underline-offset-4 transition-all hover:text-indigo-500 hover:underline"
+<Card class="min-w-sm">
+	<CardHeader>
+		<CardTitle class="scroll-m-20 text-2xl font-semibold tracking-tight">
+			{sign_in_form ? 'Sign In' : 'Create an account'}
+		</CardTitle>
+		<CardDescription>
+			{sign_in_form
+				? 'Enter your email and password below to sign in to your account'
+				: 'Enter your email and password below to register your account'}
+		</CardDescription>
+	</CardHeader>
+	<CardContent>
+		<form>
+			<Field.Set>
+				<Field.Group>
+					<Field.Field>
+						<Field.Label for="username">Email</Field.Label>
+						<Input ref={email_input} id="username" type="email" placeholder="Your email" />
+					</Field.Field>
+					<Field.Field>
+						<Field.Label for="password">Password</Field.Label>
+						<Input ref={password_input} id="password" type="password" placeholder="••••••••" />
+					</Field.Field>
+				</Field.Group>
+			</Field.Set>
+		</form>
+	</CardContent>
+	<CardFooter class="flex-col gap-2">
+		<Button onclick={onsubmit} type="submit" class="w-full cursor-pointer">Continue</Button>
+		<Button
+			variant="ghost"
+			class="w-full cursor-pointer font-normal"
+			onclick={() => (sign_in_form = !sign_in_form)}
 		>
-			{sign_in_form ? 'Sign Up' : 'Sign In'}
-		</button>
-	</p>
-</AuthForm>
+			{sign_in_form ? 'Create an account' : 'Sign in to existing account'}
+		</Button>
+	</CardFooter>
+</Card>
